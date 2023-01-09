f4f7656432f33ca41026d870c5fa233facf3cb47
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-01-09 16:27:01 +0300

    fix bugs

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,13 +4,7 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import ArrowIcon from "./arrow.svg";
 
-export const Button = ({
-  arrow = "none",
-  appearance,
-  children,
-  className,
-  ...props
-}: ButtonProps): JSX.Element => {
+export const Button = ({ arrow = "none", appearance, children, className, ...props }: ButtonProps): JSX.Element => {
   return (
     <button
       className={cn(styles.button, className, {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
-import { Htag, Button, Paragraph, Tag } from "../components";
+import { useEffect, useState } from "react";
+
+import { Htag, Button, Paragraph, Tag, Rating } from "../components";
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    console.log("Counter" + counter);
+    return function cleanUp() {
+      console.log("unmount" + counter);
+    };
+  }, []);
   return (
     <>
-      <Htag tag="h1"> </Htag>
-      <Button appearance="primary" arrow="right">
+      <Htag tag="h1">{counter} </Htag>
+      <Button appearance="primary" arrow="right" onClick={() => setCounter((x) => x + 1)}>
         Кнопка
       </Button>
-      <Button appearance="ghost" arrow="right">
+      <Button appearance="ghost" arrow="right" onClick={() => setCounter((x) => x - 1)}>
         Кнопка
       </Button>
       <Paragraph size="p14">Маленький</Paragraph>
@@ -21,6 +31,7 @@ export default function Home(): JSX.Element {
         green
       </Tag>
       <Tag color="primary">lol</Tag>
+      <Rating rating={4} />
     </>
   );
 }
