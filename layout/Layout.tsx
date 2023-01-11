713aee52c6ebe69a19f5612958fc724ee25f4ca5
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-01-11 21:47:06 +0300

    addded menu

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,25 +4,28 @@ import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { LayoutProps } from "./Layout.props";
+import { AppContextProvider, IAppContext } from "../context/app.context";
 import styles from "./Layout.module.css";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <div className={styles.wrapper}>
       <Header className={styles.header} />
-        <Sidebar className={styles.sidebar} />
-        <main className={styles.body}>{children}</main>
+      <Sidebar className={styles.sidebar} />
+      <main className={styles.body}>{children}</main>
       <Footer className={styles.footer} />
     </div>
   );
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
   return function withLayoutComaponent(props: T): JSX.Element {
     return (
-      <Layout>
-        <Component {...props} />
-      </Layout>
+      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      </AppContextProvider>
     );
   };
 };
