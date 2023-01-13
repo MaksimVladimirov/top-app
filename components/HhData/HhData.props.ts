d46a92ba5d7a4b2e4d173c8889b862e750d7065b
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-01-13 16:19:35 +0300

    added hhdata

---
+++ components/HhData/HhData.props.ts
@@ -0,0 +1,4 @@
+import { HTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+import { HeadHunterData } from "../../interfaces/page.interface";
+
+export interface HhDataProps extends HeadHunterData {}
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
+import cn from "classnames";
+
 import { SidebarProps } from "./Sidebar.props";
 import styles from "./Sidebar.module.css";
 import { Menu } from "../Menu/Menu";
+import Logo from "../logo.svg";
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <aside {...props}>
+    <aside className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>поиск</div>
       <Menu />
     </aside>
   );
