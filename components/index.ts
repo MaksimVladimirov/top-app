07c9936291a911032f9b602bee90d5f23de18b0e
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-01-13 16:39:53 +0300

    fix bugs

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,4 @@ export * from "./Paragraph/Paragraph";
 export * from "./Tag/Tag";
 export * from "./Rating/Rating";
 export * from "./Card/Card";
-export * from "./HhData/hhData"
+export * from "./HhData/HhData"
