ba7c0fa2de35294629d33a41596ee19ab18cb35d
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-03-08 16:39:36 +0300

    fix some bugs

--- hooks/useScrollY.ts
+++ hooks/useScrollY.ts
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 export const useScrollY = (): number => {
   const isBrowser = typeof window !== "undefined";
-
   const [scrollY, setScrollY] = useState<number>(0);
 
   const handleScroll = () => {
--- page-components/TopPageComponent/TopPageComponent.props.tsx
+++ page-components/TopPageComponent/TopPageComponent.props.tsx
@@ -1,8 +1,6 @@
 import { PageModel, TopLevelCategory } from "../../interfaces/page.interface";
 import { ProductModel } from "../../interfaces/product.interface";
 
-
-
 export interface TopPageComponentProps  {
   firstCategory: TopLevelCategory;
   page: PageModel ;
