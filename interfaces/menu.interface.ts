1e11050e8e5b3ada8787b3a7a590824a59e82d4a
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-03-08 17:16:54 +0300

    new interface

--- interfaces/menu.interface.ts
+++ interfaces/menu.interface.ts
@@ -21,3 +21,21 @@ export interface FirstLevelMenuItem {
   icon: JSX.Element;
   id: TopLevelCategory;
 }
+export interface FirstLevelMenuItem {
+  route: string;
+  name: string;
+  icon: JSX.Element;
+  id: TopLevelCategory;
+}
+export interface FirstLevelMenuItem {
+  route: string;
+  name: string;
+  icon: JSX.Element;
+  id: TopLevelCategory;
+}
+export interface FirstLevelMenuItem {
+  route: string;
+  name: string;
+  icon: JSX.Element;
+  id: TopLevelCategory;
+}
